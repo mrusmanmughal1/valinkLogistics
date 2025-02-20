c1f96620fe1ed6076855f1042b42498c935cbed4
Muzam Ali <muzamdty@gmail.com>
2025-02-20 00:37:19 +0000

    Todo Add

---
+++ Todo.js
@@ -0,0 +1,10 @@
+//TODO: Pagination by Job status
+//TODO: Search by Job Number
+//TODO: Emails for quotes
+
+//Front End
+//TODO: Details page for quotes
+//TODO: Fix Submit Form Issue
+//TODO: Implimnt Search and Pagination
+//TODO: Vehicle Detail change Setting
+//TODO: Price/distance bug fix
